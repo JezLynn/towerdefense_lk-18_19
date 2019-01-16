fde5a6604dabb01689389ba0c38f75d9c4bab586
sebl311 <46741993+sebl311@users.noreply.github.com>
2019-01-16 15:25:29 +0100

    brauchen wir nicht mehr

--- src/hitbox.java
+++
@@ -1,8 +0,0 @@
-public abstract class hitbox {
-pos entety;
-public Punkt getPos(){return entety.getPos();}
-
-abstract boolean enthaelt(Punkt P);
-abstract boolean ueberschneidet(hitbox H);
-hitbox(pos entety){this.entety=entety;}
-}
