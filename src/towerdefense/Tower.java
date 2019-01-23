71b8412a4bef865c62846cb1989795082f7bd7d3
CurryWaffel <46422198+CurryWaffel@users.noreply.github.com>
2019-01-23 15:17:40 +0100

    Aufräumen, Formatierung

--- src/towerdefense/Tower.java
+++ src/towerdefense/Tower.java
@@ -8,16 +8,15 @@ package towerdefense; /**
    
 import towerdefense.grafikelemente.*;
 
-
 import java.util.ArrayList;
+
 public class Tower {
-  
   // Anfang Attribute
   Playground playground;
- public Grafikelemente G;
+  public Grafikelemente G;
 
   double range=2;             //Reichweite des Turms
-  int reload=6;             //Nachladezeit des  Turms in ticks
+  int reload=6;               //Nachladezeit des  Turms in ticks
   int damage=10;              //Staerke des Turms pro Hit
   
   Punkt position;             // Punkt der die Position des Turms definiert
@@ -33,7 +32,6 @@ public class Tower {
     *
     *@param position Ein Punkt an dem der Turm erstellt wird 
     */
-  
   Tower(Punkt position,Playground playground) {
     this.position=position;
     this.playground=playground;
@@ -44,50 +42,47 @@ public class Tower {
   /**
     *wenn laden > reload (wenn lange genug nachgeladen worden ist) wird geschossen
     *
-    *@param Enemys Eine Liste von Enemys die erreicht werden k�nnen
+    *@param Enemys Eine Liste von Enemys die erreicht werden können
     */
-  public void update( ArrayList<Enemy> Enemys){
+  public void update(ArrayList<Enemy> Enemys){
     if(laden>reload){
-
-      if(target==null || !target.aktiv){
-          ArrayList<Enemy> Enemyinrange=inrange(Enemys);
-          if(Enemyinrange!=null){target=Enemyinrange.get(0);}
-
-
+      if(target == null || !target.aktiv || inrange(target)){
+          ArrayList<Enemy> Enemyinrange = inrange(Enemys);
+          if(Enemyinrange!=null) { target=Enemyinrange.get(0); }
       }
-      if(target!=null && inrange(target)){
+     
+      if(target!=null && inrange(target)) {
         shoot();
         playground.Particles.add(new Particle(10,new Line(position,target.position.copy())));
         laden=0;
-      }                                                     //TODO
-      
-    }else {laden++;}
+      }                                                      
+    } else { laden++; }
   }
   public void shoot(){
-    target.HP-=damage;
+    target.HP -= damage;
     System.out.println(target.HP);
     target.update();
   }
 
   
   /**
-    *Gibt eine Liste zurueck, die aus den Enemys besteht die ein Tower von seiner Position aus mit seinem Radius erreichen kann.
+    * Gibt eine Liste zurueck, die aus den Enemys besteht,
+    * die ein Tower von seiner Position aus mit seinem Radius erreichen kann.
     *
     *@param Enemys Eine Liste aller Enemys auf dem Playground
     *@return ArrayList<Enemy> Eine Liste aller Enemys die erreichbar sind
     */
-  
   public ArrayList<Enemy> inrange( ArrayList<Enemy> Enemys){
-    ArrayList<Enemy> Enemyinrange=new ArrayList<Enemy>();
+    ArrayList<Enemy> Enemyinrange = new ArrayList<Enemy>();
     for (Enemy enemy : Enemys) {
-      if (enemy.position.distance(this.position) < this.range) Enemyinrange.add(enemy);
+      if (enemy.position.distance(this.position) < this.range) { Enemyinrange.add(enemy) };
     } // end of for
-if(Enemyinrange.size()==0){return null;}
-    return Enemyinrange;
+    if(Enemyinrange.size()==0) { return null; }
+    else { return Enemyinrange;}
   }
   
   /**
-    *Gibt wahr zurueck, wenn ein Enemy von einer Position
+    * Gibt wahr zurueck, wenn ein Enemy von einer innerhalb der Reichweite des Turms ist
     *
     *@param E zu ueberpruefender Enemy
     *@return boolean liegt E in der Reichweite des Turms?
@@ -96,4 +91,4 @@ if(Enemyinrange.size()==0){return null;}
     return (E.aktiv && E.position.distance(this.position)<this.range );
   }
   // Ende Methoden
-} // end of Turm
+} // end of Tower
